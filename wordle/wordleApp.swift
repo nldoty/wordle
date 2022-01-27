616c7a18fbe82016525fa987afc65ebdd31111e5
Nick Doty <doty.nick@gmail.com>
2022-01-26 21:12:36 -0500

    Initial Commit

---
+++ wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by Nicholas Doty on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
